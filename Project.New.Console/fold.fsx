a7a13bae18ed39f9ee21c60d6f68f834f48358a2
Roman Motyka <r.motyka@cctechnology.pl>
2017-09-06 17:58:12 +0100

    list stuff

--- Project.New.Console/fold.fsx
+++ Project.New.Console/fold.fsx
@@ -8,3 +8,12 @@ let where predicate numbers =
 
 let pred1 elem =
     if elem > 3 then true else false
+    
+/////////// foldBack
+List.fold (fun acc x -> printf "%d " x ) () [1..10]
+List.foldBack (fun x acc -> printf "%d " x ) [1..10] ()
+
+////////// fold2
+
+List.fold2 (fun acc x y -> printf "%d%s " x y) () [1..3] ["a"; "b"; "c"]
+List.foldBack2 (fun y x acc -> printf "%d%s " x y)  ["a"; "b"; "c"] [1..3] ()
