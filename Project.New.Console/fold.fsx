7301d0d639c975182de4399d4ce3cf08d349b0e3
Roman Motyka <r.motyka@cctechnology.pl>
2017-09-07 12:27:26 +0100

    list module to choose

--- Project.New.Console/fold.fsx
+++ Project.New.Console/fold.fsx
@@ -15,5 +15,20 @@ List.foldBack (fun x acc -> printf "%d " x ) [1..10] ()
 
 ////////// fold2
 
-List.fold2 (fun acc x y -> printf "%d%s " x y) () [1..3] ["a"; "b"; "c"]
-List.foldBack2 (fun y x acc -> printf "%d%s " x y)  ["a"; "b"; "c"] [1..3] ()
+List.fold2 (fun acc x y -> printf "%d%O " x y) () [1..3] ['a'..'c']
+List.foldBack2 (fun y x acc -> printf "%d%O " x y)  ['a'..'c'] [1..3] ()
+
+///////// append
+
+let list1 = List.append [1; 2] [3; 4]
+let list2 = List.concat [ [5; 6 ]; [7; 8] ]
+let list3 = list1 @ list2
+
+// average
+
+List.map (float) [0; 1; 1; 2] |> List.average 
+
+
+// choose
+
+List.choose (fun x -> x) [ Some(2); None;  Some(3);  None; Some(4)]
