fdc0462276e8d0fbad467eaea034bd782e3875cd
Roman Motyka <r.motyka@cctechnology.pl>
2017-09-11 16:10:09 +0200

    Test main function

--- Multidata.Stv.Test/StvCalculatorTest.fs
+++ Multidata.Stv.Test/StvCalculatorTest.fs
@@ -2,6 +2,7 @@ module Multidata.Stv.Test.StvCalculator
 
 open System
 open Xunit
+open Multidata.Stv.StvModels
 open Multidata.Stv.StvCalculator
 
 [<Fact>]
@@ -15,9 +16,13 @@ let ``droop quota`` () =
 
 [<Fact>]
 let ``mainCaluclation`` () =
-    let candidates = [{1; "Smith"}; {2;"Gordon"}]
+    let candidate1 = {Candidate.candidateId = 1; Candidate.name = "Smith"}
+    let candidate2 = {Candidate.candidateId = 2; Candidate.name = "Gordon"}
+    let candidates = [candidate1; candidate2]
     let poll = {numberOfSeats = 12; candidates = candidates}
-    let voteList = [[{1;1}]]
+    let voteList = [[{candidateId = 1; preference = 1}]]
     let res = mainCaluclation poll voteList
 
-    Assert.Equal()
+    let items = res.items
+
+    Assert.Equal(1, items.Item(0).candidateId)
